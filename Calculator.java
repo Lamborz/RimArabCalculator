77ad7ffc04e8f7b46b5ab001e4836a5de3909909
Lamborz <geniusessay@gmail.com>
2020-10-12 20:34:17 +0500

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -7,10 +7,10 @@ import java.io.InputStreamReader;
 
 public class Calculator {
     public static void main(String[] args) throws IOException {
-        
-        
+
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-            while(true){
+
+        while (true) {
             String line = reader.readLine();
             char[] chars = line.toCharArray();
             ConvertNumbers rimNums = new ConvertNumbers();
@@ -19,6 +19,14 @@ public class Calculator {
             int result;
             int arabFirst;
             int arabSecond;
+            int firstDigOfTheFirstNum;
+            int secondDigOfTheFirstNum;
+            int firstDigOfTheSecondNum;
+            int secondDigOfTheSecondNum;
+            String firstDigOfTheFirstRimNum;
+            String secondDigOfTheFirstRimNum;
+            String firstDigOfTheSecondRimNum;
+            String secondDigOfTheSecondRimNum;
 
             for (int i = 0; i < chars.length; i++) {
 
@@ -34,6 +42,22 @@ public class Calculator {
                     if (chars[i] == '*') {
                         arabFirst = Integer.parseInt(line.substring(0, i));
                         arabSecond = Integer.parseInt(line.substring(i + 1, line.length()));
+
+                        //если число двухзначное, то узнаём из каких цифр остоит первое цисло
+                        if(line.substring(0,i).length() >=2){
+                            firstDigOfTheFirstNum = Integer.parseInt(line.substring(0,1));
+                            secondDigOfTheFirstNum = Integer.parseInt(line.substring(1,2));
+                            System.out.println(firstDigOfTheFirstNum+" , " + secondDigOfTheFirstNum);
+                        }
+
+                        //если второе число двухзначное, то узнаём из каких цифр остоит второе цисло
+                        if(line.substring(i + 1, line.length()).length() >= 2){
+                            String s  = line.substring(i + 1, line.length()).substring(0,1);
+                            firstDigOfTheSecondNum = Integer.parseInt(s);
+                            String s2  = line.substring(i + 1, line.length()).substring(1,2);
+                            secondDigOfTheSecondNum = Integer.parseInt(s2);
+                            System.out.println(firstDigOfTheSecondNum+" , " + secondDigOfTheSecondNum);
+                        }
                         result = arabFirst * arabSecond;
                         System.out.println(result);
                         break;
@@ -43,77 +67,185 @@ public class Calculator {
                         arabSecond = Integer.parseInt(line.substring(i + 1, line.length()));
                         result = arabFirst / arabSecond;
                         System.out.println(result);
-                        break;
-                    } else if (chars[i] == '+') {
+
+                    }  if (chars[i] == '+') {
                         arabFirst = Integer.parseInt(line.substring(0, i));
                         arabSecond = Integer.parseInt(line.substring(i + 1, line.length()));
                         result = arabFirst + arabSecond;
                         System.out.println(result);
                         break;
                     }
-                    if (chars[i] == '-') {
-                        arabFirst = Integer.parseInt(line.substring(0, i));
-                        arabSecond = Integer.parseInt(line.substring(i + 1, line.length()));
-                        result = arabFirst - arabSecond;
-                        System.out.println(result);
+
+                }
+                if (chars[i] == '+') {
+                    rimFirst = line.substring(0, i);
+                    rimSecond = line.substring(i + 1, line.length());
+
+                    arabFirst = rimNums.convertRimToArab(rimFirst);
+                    arabSecond = rimNums.convertRimToArab(rimSecond);
+                    result = arabFirst + arabSecond;
+
+                    if(result > 10) {
+                        if(result ==100){
+                            System.out.println("C");
+                            break;
+                        }
+
+                        String strResult = String.valueOf(result);
+                        String firstResultDig = strResult.substring(0,1);
+                        String secondResultDig = strResult.substring(1,2);
+                        int firstNum = Integer.parseInt(firstResultDig);
+                        int secondNum = Integer.parseInt(secondResultDig);
+                        if(secondNum==0){
+                            rimSecond = "";
+                        } else {
+                            rimSecond = rimNums.convertArabToRimForSecondDig(secondNum);
+                        }
+                        rimFirst = rimNums.convertArabToRimFirst(firstNum);
+                        System.out.println(rimFirst+rimSecond);
                         break;
                     }
-                    if (chars[i] == '*') {
-                        arabFirst = Integer.parseInt(line.substring(0, i));
-                        arabSecond = Integer.parseInt(line.substring(i + 1, line.length()));
-                        result = arabFirst * arabSecond;
-                        System.out.println(result);
+                    String res = rimNums.convertArabToRimForSecondDig(result);
+                    System.out.println(res);
+                    break;
+//                    rimFirst = line.substring(0, i);
+//                    rimSecond = line.substring(i + 1, line.length());
+//
+//                    arabFirst = rimNums.convertRimToArab(rimFirst);
+//                    arabSecond = rimNums.convertRimToArab(rimSecond);
+//
+//                    result = arabFirst + arabSecond;
+////                    String res = rimNums.convertArabToRim(result);
+////                    System.out.println(res);
+//                    break;
+                }
+                if (chars[i] == '-') {
+                    rimFirst = line.substring(0, i);
+                    rimSecond = line.substring(i + 1, line.length());
+
+                    arabFirst = rimNums.convertRimToArab(rimFirst);
+                    arabSecond = rimNums.convertRimToArab(rimSecond);
+                    result = arabFirst - arabSecond;
+
+                    if(result > 10) {
+                        if(result == 100){
+                            System.out.println("C");
+                            break;
+                        }
+
+                        String strResult = String.valueOf(result);
+                        String firstResultDig = strResult.substring(0,1);
+                        String secondResultDig = strResult.substring(1,2);
+                        int firstNum = Integer.parseInt(firstResultDig);
+                        int secondNum = Integer.parseInt(secondResultDig);
+                        if(secondNum==0){
+                            rimSecond = "";
+                        } else {
+                            rimSecond = rimNums.convertArabToRimForSecondDig(secondNum);
+                        }
+                        rimFirst = rimNums.convertArabToRimFirst(firstNum);
+                        System.out.println(rimFirst+rimSecond);
                         break;
                     }
-                    if (chars[i] == '/') {
-                        arabFirst = Integer.parseInt(line.substring(0, i));
-                        arabSecond = Integer.parseInt(line.substring(i + 1, line.length()));
-                        result = arabFirst / arabSecond;
-                        System.out.println(result);
+                    String res = rimNums.convertArabToRimForSecondDig(result);
+                    System.out.println(res);
+                    break;
+                }
+                if (chars[i] == '*') {
+//
+//                    //если число двухзначное, то узнаём из каких цифр остоит первое цисло
+//                    if(line.substring(0,i).length() >=2){
+//                        firstDigOfTheFirstRimNum = line.substring(0,1);
+//
+//                        secondDigOfTheFirstRimNum = line.substring(1,2);
+//                        System.out.println(firstDigOfTheFirstRimNum+" , " + secondDigOfTheFirstRimNum);
+//                    }
+//
+//                    //если второе число двухзначное, то узнаём из каких цифр остоит второе цисло
+//                    if(line.substring(i + 1, line.length()).length() >= 2) {
+//                        firstDigOfTheSecondRimNum  = line.substring(i + 1, line.length()).substring(0,1);
+//                        int chekFirstNum = rimNums.convertRimToArab(firstDigOfTheSecondRimNum);
+//                        String firstRimDigOfTheSecondRimNum = rimNums.convertingArabToRomeFromm10To100ForFirstArabDig(chekFirstNum);
+//                        secondDigOfTheSecondRimNum  = line.substring(i + 1, line.length()).substring(1,2);
+//                        int chekSecondNum = rimNums.convertRimToArab(secondDigOfTheSecondRimNum);
+////                        String secondRimDigOfTheSecondRimNum =rimNums.convertArabToRim(chekSecondNum);
+////                        System.out.println(firstRimDigOfTheSecondRimNum+" , " + secondRimDigOfTheSecondRimNum);
+//                    }
+                    rimFirst = line.substring(0, i);
+                    rimSecond = line.substring(i + 1, line.length());
+
+                    arabFirst = rimNums.convertRimToArab(rimFirst);
+                    arabSecond = rimNums.convertRimToArab(rimSecond);
+                    result = arabFirst * arabSecond;
+
+                    if(result > 10) {
+                        if(result ==100){
+                            System.out.println("C");
+                            break;
+                        }
+
+                    String strResult = String.valueOf(result);
+                    String firstResultDig = strResult.substring(0,1);
+                    String secondResultDig = strResult.substring(1,2);
+                    int firstNum = Integer.parseInt(firstResultDig);
+                    int secondNum = Integer.parseInt(secondResultDig);
+                    if(secondNum==0){
+                        rimSecond = "";
+                    } else {
+                        rimSecond = rimNums.convertArabToRimForSecondDig(secondNum);
+                    }
+                    rimFirst = rimNums.convertArabToRimFirst(firstNum);
+                        System.out.println(rimFirst+rimSecond);
                         break;
                     }
+                   String res = rimNums.convertArabToRimForSecondDig(result);
+                    System.out.println(res);
+                    break;
+
                 }
-                if(chars[i]=='+'){
-                    rimFirst = line.substring(0,i);
-                    rimSecond = line.substring(i+1,line.length());
+                if (chars[i] == '/') {
+//                    rimFirst = line.substring(0, i);
+//                    rimSecond = line.substring(i + 1, line.length());
+//                    arabFirst = rimNums.convertRimToArab(rimFirst);
+//                    arabSecond = rimNums.convertRimToArab(rimSecond);
+//                    result = arabFirst / arabSecond;
+////                    String res = rimNums.convertArabToRim(result);
+////                    System.out.println(res);
+//                    break;
+                    rimFirst = line.substring(0, i);
+                    rimSecond = line.substring(i + 1, line.length());
+
                     arabFirst = rimNums.convertRimToArab(rimFirst);
                     arabSecond = rimNums.convertRimToArab(rimSecond);
-                    result = arabFirst+ arabSecond;
-                    String res = rimNums.convertArabToRim(result);
+                    result = arabFirst / arabSecond;
+
+                    if(result > 10) {
+                        if(result ==100){
+                            System.out.println("C");
+                            break;
+                        }
+
+                        String strResult = String.valueOf(result);
+                        String firstResultDig = strResult.substring(0,1);
+                        String secondResultDig = strResult.substring(1,2);
+                        int firstNum = Integer.parseInt(firstResultDig);
+                        int secondNum = Integer.parseInt(secondResultDig);
+                        if(secondNum==0){
+                            rimSecond = "";
+                        } else {
+                            rimSecond = rimNums.convertArabToRimForSecondDig(secondNum);
+                        }
+                        rimFirst = rimNums.convertArabToRimFirst(firstNum);
+                        System.out.println(rimFirst+rimSecond);
+                        break;
+                    }
+                    String res = rimNums.convertArabToRimForSecondDig(result);
                     System.out.println(res);
                     break;
                 }
-                if(chars[i]=='-'){
-                    rimFirst = line.substring(0,i);
-                    rimSecond = line.substring(i+1,line.length());
-                    arabFirst = rimNums.convertRimToArab(rimFirst);
-                    arabSecond = rimNums.convertRimToArab(rimSecond);
-                    result = arabFirst- arabSecond;
-                    String res = rimNums.convertArabToRim(result);
-                    System.out.println(res);
-                    break;
-                }
-                if(chars[i]=='*'){
-                    rimFirst = line.substring(0,i);
-                    rimSecond = line.substring(i+1,line.length());
-                    arabFirst = rimNums.convertRimToArab(rimFirst);
-                    arabSecond = rimNums.convertRimToArab(rimSecond);
-                    result = arabFirst* arabSecond;
-                    String res = rimNums.convertArabToRim(result);
-                    System.out.println(res);
-                    break;
-                }
-                if(chars[i]=='/'){
-                    rimFirst = line.substring(0,i);
-                    rimSecond = line.substring(i+1,line.length());
-                    arabFirst = rimNums.convertRimToArab(rimFirst);
-                    arabSecond = rimNums.convertRimToArab(rimSecond);
-                    result = arabFirst/ arabSecond;
-                    String res = rimNums.convertArabToRim(result);
-                    System.out.println(res);
-                    break;
-                }
+
             }
-            }
-    }
+        }
+        }
+
 }
